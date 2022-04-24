7cef4a0afc4f462bd0e63b9a00bea39a522b6b3a
Mariia <gestia29@gmail.com>
2022-04-24 14:32:46 +0300

    Занятие 1: Основы JavaScript

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  let newStr = str.toLowerCase();
+  let firstCheck = '1xBet'.toLowerCase();
+  let secondCheck = 'XXX'.toLowerCase();
+  return (newStr.includes(firstCheck) || newStr.includes(secondCheck));
 }
