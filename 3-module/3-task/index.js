1aad138140be718d3bde21c6e67c4ebffc68cf10
Mariia <gestia29@gmail.com>
2022-05-10 20:49:17 +0300

    Занятие 3: Функции и массивы

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let arrChange = str.split('-');
+  let arrNew = arrChange.map((item, index) =>
+    index === 0
+      ? item
+      : item[0].toUpperCase() + item.slice(1)
+  );
+  return arrNew.join('');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+    .split(' ')
+    .map(item => parseFloat(item))
+    .filter(item => typeof item === 'number' && isFinite(item));
+  let max = Math.max.apply(null, arr);
+  let min = Math.min.apply(null, arr);
+  return {
+    min: min,
+    max: max
+  };
 }
