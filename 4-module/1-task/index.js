03ad5a32af72c20228277baf0e89226d144c0e4e
Mariia <gestia29@gmail.com>
2022-05-15 22:57:13 +0300

    Занятие 4: Знакомство с DOM

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('UL');
+  for (let item of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    list.appendChild(li);
+  }
+  return list;
 }
